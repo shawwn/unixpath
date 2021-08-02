62e01225c796c66648e727f92efb9fa4ade001b4
Shawn Presser <shawnpresser@gmail.com>
2021-08-01 23:45:28 -0500

    Add more tests

--- tests/test_unixpath.py
+++ tests/test_unixpath.py
@@ -1,8 +1,10 @@
 import unixpath
 import os.path
 
+
 def check(fn, *args, **kws):
     assert getattr(os.path, fn)(*args, **kws) == getattr(unixpath, fn)(*args, **kws)
+
 
 def test_path():
     check('join', '.', '/foo')
@@ -21,3 +23,12 @@ def test_path():
     assert '/' == unixpath.normpath('////')
     assert '///' == unixpath.dirname('///')
     assert '////' == unixpath.dirname('////')
+    assert 'a/b/../c' == unixpath.join('a', 'b', '..', 'c')
+    assert 'a/c' == unixpath.normpath(unixpath.join('a', 'b', '..', 'c'))
+    assert 'a/b' == unixpath.dirname('a/b/c')
+    assert 'c' == unixpath.basename('a/b/c')
+    assert '' == unixpath.basename('a/b/c/')
+    assert '' == unixpath.basename('')
+    assert '.' == unixpath.normpath('')
+    assert unixpath.isabs('/foo')
+    assert not unixpath.isabs('./foo')
